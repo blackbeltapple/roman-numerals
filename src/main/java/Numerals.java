fad5fb650ce23a832f6f93d202021f08aa1bd36f
Amanda Manekshaw <amanda.manekshaw@deepskybluesolutions.com>
2019-01-11 13:02:46 +0000

    Initial commit - BBC roman numerals challenge

---
+++ src/main/java/Numerals.java
@@ -0,0 +1,79 @@
+public class Numerals implements RomanNumeralGenerator {
+
+    public static int SUBTRACTIVE_DIGITS = 4;
+
+    public String generate(int number) throws Exception {
+
+
+        if (number < 1 || number > 3999) {
+            throw new IllegalArgumentException("Valid arguments are 1 to 3999.");
+        }
+
+        SymbolSet thousands = new SymbolSet("M", "", "", 1000);
+        SymbolSet hundreds = new SymbolSet("C", "D", "M", 100);
+        SymbolSet tens = new SymbolSet("X", "L", "C", 10);
+        SymbolSet decimals = new SymbolSet("I", "V", "X", 1);
+
+        return convertDigit(number, thousands)
+                + convertDigit(number, hundreds)
+                + convertDigit(number, tens)
+                + convertDigit(number, decimals);
+    }
+
+    String convertDigit(int number, SymbolSet symbolSet) {
+
+        int digit = (number / symbolSet.divisor) % 10;
+        int quotient = digit / 5;
+        int remainder = digit % 5;
+
+        String romanNumeral = addMultipleSymbols(symbolSet.midSymbol, quotient) + addMultipleSymbols(symbolSet.baseSymbol, remainder);
+
+        romanNumeral = convertToSubtractiveNotation(romanNumeral, symbolSet, quotient);
+
+        return romanNumeral;
+    }
+
+    String convertToSubtractiveNotation(String simpleRomanNumeral, SymbolSet symbolSet, int quotient) {
+        if (!simpleRomanNumeral.contains(addMultipleSymbols(symbolSet.baseSymbol, SUBTRACTIVE_DIGITS))) {
+            return simpleRomanNumeral;
+        }
+
+        String toReplace;
+        String replaceBy;
+
+        if (quotient > 0) {
+            toReplace = symbolSet.midSymbol + addMultipleSymbols(symbolSet.baseSymbol, SUBTRACTIVE_DIGITS);
+            replaceBy = symbolSet.baseSymbol + symbolSet.upperSymbol;
+
+        } else {
+            toReplace = addMultipleSymbols(symbolSet.baseSymbol, SUBTRACTIVE_DIGITS);
+            replaceBy = symbolSet.baseSymbol + symbolSet.midSymbol;
+        }
+
+        return simpleRomanNumeral.replace(toReplace, replaceBy);
+
+    }
+
+    String addMultipleSymbols(String symbol, int multiple) {
+        String symbols = "";
+        for (int i = 0; i < multiple; i++) {
+            symbols = symbols.concat(symbol);
+        }
+        return symbols;
+    }
+}
+
+class SymbolSet {
+
+    String baseSymbol;
+    String midSymbol;
+    String upperSymbol;
+    int divisor;
+
+    SymbolSet( String baseSymbol, String midSymbol, String upperSymbol, int divisor) {
+        this.baseSymbol = baseSymbol;
+        this.midSymbol = midSymbol;
+        this.upperSymbol = upperSymbol;
+        this.divisor = divisor;
+    }
+}
---
+++ src/test/java/NumeralsTest.java
@@ -0,0 +1,114 @@
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+@RunWith(Enclosed.class)
+public class NumeralsTest {
+
+    @RunWith(Parameterized.class)
+    public static class ParameterizedTests {
+
+        @Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][] {
+                    //The first item in the array is the input, and second is the expected outcome.
+                    { 1, "I" },
+                    { 2, "II" },
+                    { 3, "III" },
+                    { 4, "IV" },
+                    { 5, "V" },
+                    { 6, "VI" },
+                    { 7, "VII" },
+                    { 8, "VIII" },
+                    { 9, "IX" },
+                    { 10, "X" },
+                    { 11, "XI" },
+                    { 12, "XII" },
+                    { 13, "XIII" },
+                    { 14, "XIV" },
+                    { 15, "XV" },
+                    { 16, "XVI" },
+                    { 17, "XVII" },
+                    { 18, "XVIII" },
+                    { 19, "XIX" },
+                    { 20, "XX" },
+                    { 24, "XXIV" },
+                    { 29, "XXIX" },
+                    { 30, "XXX" },
+                    { 40, "XL" },
+                    { 44, "XLIV" },
+                    { 49, "XLIX" },
+                    { 50, "L" },
+                    { 60, "LX" },
+                    { 70, "LXX" },
+                    { 80, "LXXX" },
+                    { 90, "XC" },
+                    { 99, "XCIX" },
+                    { 100, "C" },
+                    { 400, "CD" },
+                    { 500, "D" },
+                    { 900, "CM" },
+                    { 1000, "M" },
+                    { 1776, "MDCCLXXVI" },
+                    { 1954, "MCMLIV" },
+                    { 1990, "MCMXC" },
+                    { 2000, "MM" },
+                    { 2014, "MMXIV" },
+                    { 3000, "MMM" },
+                    { 3999, "MMMCMXCIX" }
+            });
+        }
+
+        private int input;
+        private String expected;
+
+        public ParameterizedTests(int input, String expected) {
+            this.input = input;
+            this.expected = expected;
+        }
+
+        @Test
+        public void test() throws Exception {
+            Numerals numerals = new Numerals();
+            assertThat(numerals.generate(input), is(equalTo(expected)));
+        }
+
+    }
+
+    public static class IndividualTests {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void testArgumentLessThan1ThrowsException() throws Exception {
+            Numerals numerals = new Numerals();
+            numerals.generate(0);
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void testArgumentGreaterThan3999ThrowsException() throws Exception {
+            Numerals numerals = new Numerals();
+            numerals.generate(4000);
+        }
+
+        @Test
+        public void testAddSymbols() {
+            Numerals numerals = new Numerals();
+            assertThat(numerals.addMultipleSymbols("X", 4), is(equalTo("XXXX")));
+        }
+
+        @Test
+        public void testAddSymbolsForZeroQuantity() {
+            Numerals numerals = new Numerals();
+            assertThat(numerals.addMultipleSymbols("X", 0), is(equalTo("")));
+        }
+    }
+}
+
